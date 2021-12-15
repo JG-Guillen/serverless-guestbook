7bde349f93960803d068925539b33c85d82de6d9
JG-Guillen <38336339+JG-Guillen@users.noreply.github.com>
2021-12-15 00:31:33 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f0ffc487.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
